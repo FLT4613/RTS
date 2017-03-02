f954b589121860bda9333b58804dfcef5e05f0c3
FLT <ft4613st@gmail.com>
2017-03-02 18:21:51 +0900

    マウスオーバーで拡大

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,25 +6,34 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import flixel.group.FlxSpriteGroup;
 
+import flixel.input.mouse.FlxMouseEventManager;
 import objects.Character;
 
-class PlayState extends FlxState
-{
-	var player:Character;
-	override public function create():Void
-	{
+class PlayState extends FlxState{
+	var characterPool:FlxSpriteGroup;
+
+	override public function create():Void{
 		super.create();
-		player=new Character();
-		add(player);
+		characterPool=new FlxSpriteGroup();
+		for(i in 0...9){
+			var character=new Character(FlxG.random.int(50,FlxG.width-50),FlxG.random.int(50,FlxG.height-50));
+			characterPool.add(character);
+			FlxMouseEventManager.add(character,null,null,onMouseOver,onMouseOut); 
+		}
+		add(characterPool);
 	}
 
-	override public function update(elapsed:Float):Void
-	{
+	override public function update(elapsed:Float):Void{
 		super.update(elapsed);
-		if(FlxG.keys.pressed.W)player.y--;
-		if(FlxG.keys.pressed.A)player.x--;
-		if(FlxG.keys.pressed.S)player.y++;
-		if(FlxG.keys.pressed.D)player.x++;
+	}
+
+	public function onMouseOver(character:Character){
+		character.setGraphicSize(character.graphic.width*2,character.graphic.height*2);
+	}
+
+	public function onMouseOut(character:Character){
+		character.setGraphicSize(Std.int(character.width),Std.int(character.height));
 	}
 }
--- source/objects/Character.hx
+++ source/objects/Character.hx
@@ -2,10 +2,11 @@ package objects;
 import flixel.FlxSprite;
 
 class Character extends FlxSprite{
-  override public function new():Void{
-    super();
+  override public function new(x:Float,y:Float):Void{
+    super(x,y);
     makeGraphic(16, 16, 0xFFFFFFFF);
   }
+  
   override public function update(elapsed:Float):Void{
     super.update(elapsed);
   }
