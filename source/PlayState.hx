11124037f22b7968a3fd13ff3607433a2c1a89c1
FLT <ft4613st@gmail.com>
2017-03-02 23:27:06 +0900

    CallbackをCharacterに持たせる

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -20,20 +20,12 @@ class PlayState extends FlxState{
 		for(i in 0...9){
 			var character=new Character(FlxG.random.int(50,FlxG.width-50),FlxG.random.int(50,FlxG.height-50));
 			characterPool.add(character);
-			FlxMouseEventManager.add(character,null,null,onMouseOver,onMouseOut); 
+			FlxMouseEventManager.add(character,null,null,character.onMouseOver,character.onMouseOut); 
 		}
 		add(characterPool);
 	}
 
 	override public function update(elapsed:Float):Void{
 		super.update(elapsed);
-	}
-
-	public function onMouseOver(character:Character){
-		character.setGraphicSize(character.graphic.width*2,character.graphic.height*2);
-	}
-
-	public function onMouseOut(character:Character){
-		character.setGraphicSize(Std.int(character.width),Std.int(character.height));
 	}
 }
--- source/objects/Character.hx
+++ source/objects/Character.hx
@@ -6,8 +6,16 @@ class Character extends FlxSprite{
     super(x,y);
     makeGraphic(16, 16, 0xFFFFFFFF);
   }
-  
+
   override public function update(elapsed:Float):Void{
     super.update(elapsed);
   }
+
+	public function onMouseOver(character:Character){
+		setGraphicSize(character.graphic.width*2,character.graphic.height*2);
+	}
+
+	public function onMouseOut(character:Character){
+		character.setGraphicSize(Std.int(character.width),Std.int(character.height));
+	}
 }
