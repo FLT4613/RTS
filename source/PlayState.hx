2f3b9398d667534f56a51087d98882b71f9b7f4a
FLT <ft4613st@gmail.com>
2017-03-03 03:13:48 +0900

    キャラクターの矩形選択

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,6 +6,8 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import flixel.math.FlxRect;
+import flixel.math.FlxPoint;
 import flixel.group.FlxSpriteGroup;
 
 import flixel.input.mouse.FlxMouseEventManager;
@@ -17,18 +19,49 @@ class PlayState extends FlxState{
 	 */
 	var characterPool:FlxSpriteGroup;
 
+	/**
+	 * 選択範囲の矩形
+	 */
+	var selectedRange:FlxSprite;
+
+	/**
+	 * 選択範囲の始点
+	 */
+	var selectedRangeStartPos:FlxPoint;
+
 	override public function create():Void{
 		super.create();
 		characterPool=new FlxSpriteGroup();
+		selectedRange=new FlxSprite(0,0);
+		selectedRange.makeGraphic(FlxG.width,FlxG.height,0x66FFFFFF);
+		selectedRange.kill();
+
 		for(i in 0...9){
 			var character=new Character(FlxG.random.int(50,FlxG.width-50),FlxG.random.int(50,FlxG.height-50));
 			characterPool.add(character);
 			FlxMouseEventManager.add(character,null,null,character.onMouseOver,character.onMouseOut); 
 		}
 		add(characterPool);
+		add(selectedRange);
 	}
 
 	override public function update(elapsed:Float):Void{
+		if(FlxG.mouse.justPressed){
+			selectedRange.revive();
+			selectedRange.clipRect=FlxRect.weak();
+			selectedRangeStartPos=FlxG.mouse.getPosition();
+		}
+		if(FlxG.mouse.pressed){
+			selectedRange.clipRect=FlxRect.weak(
+				(FlxG.mouse.x>selectedRangeStartPos.x)?selectedRangeStartPos.x:FlxG.mouse.x,
+				(FlxG.mouse.y>selectedRangeStartPos.y)?selectedRangeStartPos.y:FlxG.mouse.y,
+				Std.int(Math.abs(FlxG.mouse.x-selectedRangeStartPos.x)),
+				Std.int(Math.abs(FlxG.mouse.y-selectedRangeStartPos.y))
+			);
+		}
+		if(FlxG.mouse.justReleased){
+			selectedRange.kill();
+		}
 		super.update(elapsed);
 	}
 }
